16ceeb7c1c83bee0994960a6acea1d748d4da21f
y4m4t0 <argun63901@gmail.com>
2019-10-19 08:32:27 +0600

    Проверка

---
+++ queue.py
@@ -0,0 +1,46 @@
+class Queue():
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def clear(self):
+        self.items.clear()
+
+    def front(self):
+        return self.items[len(self.items) - 1]
+
+q = Queue()
+
+while True:
+    a = input().split()
+
+    if(a[0] == 'exit'):
+        print('bye')
+        break
+    elif(a[0] == 'push'):
+        q.enqueue(int(a[1]))
+        print('ok')
+    elif(a[0] == 'pop'):
+        print(q.dequeue())
+    elif(a[0] == 'front'):
+        print(q.front())
+    elif(a[0] == 'clear'):
+        q.clear()
+        print('ok')
+    elif(a[0] == 'size'):
+        print(q.size())
+    else:
+        break
+
+        
---
+++ sys.py
@@ -0,0 +1,9 @@
+while True:
+    a = input().split()
+
+    if(a[0] == 'exit'):
+        break
+    elif(a[0] == 'push'):
+        if(float(a[1]) == 1):
+            print('excellent')
+print('asd')
